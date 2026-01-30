9e2f0b33fa90e90ef42a314ede0071fdae586b81
walchhoferG <8gibii@gmail.com>
2026-01-31 00:30:07 +0100

    vision cone for player

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,24 +2,33 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var occluder := $PointLight2D/LightOccluder2D
 
+const TARGET_Y_SCALE := 2.0
+const DURATION := 10.0
+
+var elapsed := 0.0
+var start_scale_y := 1.0
+
+func _ready():
+	start_scale_y = occluder.scale.y
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	if elapsed < DURATION:
+		elapsed += delta
+		var t := elapsed / DURATION
+		occluder.scale.y = lerp(start_scale_y, TARGET_Y_SCALE, t)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	velocity = Vector2(0,0)
+	look_at(get_global_mouse_position())
 	var direction := Input.get_axis("Left", "Right")
+	var up_down := Input.get_axis("Up", "Down")
 	if direction:
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y = up_down * SPEED
+	elif up_down:
+		velocity.x = direction * SPEED
+		velocity.y = up_down * SPEED
+	
 	move_and_slide()
