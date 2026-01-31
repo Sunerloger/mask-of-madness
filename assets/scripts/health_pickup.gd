c75f8518d3965427c72d8075b6951c9edbdbcb86
Elias Anton Rozanics <e12122077@student.tuwien.ac.at>
2026-01-31 18:07:00 +0100

    feat: remove debug printing from health spawner

--- assets/scripts/health_pickup.gd
+++ assets/scripts/health_pickup.gd
@@ -2,10 +2,7 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("reached 1")
-	print("entered:", body.name, " groups:", body.get_groups())
 	if body.is_in_group("player"):
-		print("reached 2")
 		var health := body.get_node("Health")
 		health.update_current_health(5)
 		queue_free()
