a73a00738219182689343f98ea04effd50501111
SteveVasi <64739595+SteveVasi@users.noreply.github.com>
2026-01-31 19:29:27 +0100

    refactor enemy base and enemies
    made code more reusable by moving methods in to the base
    added a roamer enemy test
    changed teleportation enemy to stop moving when he is being looked at the player

--- assets/scripts/enemy_base.gd
+++ assets/scripts/enemy_base.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
 class_name EnemyBase
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var PLAYER_LOOK_ANGLE_THRESHOLD = 0.3
 
 func get_strength() -> int:
 	push_error("get_strength() not implemented")
 	return 0
+
+func getDirectionToPlayer():
+	return (player.global_position - global_position).normalized()
+
+func getDirectionFromPlayer():
+	return -getDirectionToPlayer()	
+
+func getDistanceToPlayer():
+	return global_position.distance_to(player.global_position)
+
+func playerIsLooking():
+	var look_cos_angle = player.facing_direction.dot(getDirectionFromPlayer())
+	return look_cos_angle > PLAYER_LOOK_ANGLE_THRESHOLD
--- assets/scripts/roamerEnemy.gd
+++ assets/scripts/roamerEnemy.gd
@@ -1,13 +1,11 @@
 extends EnemyBase
 
-# movememnt speed of enemy
+# movement speed of enemy
 const SPEED = 200.0
 # radius at which the enemy circles the player
 const RADIUS = 200
 
 @export var strength: int = 5
-
-@onready var player = get_tree().get_first_node_in_group("player")
 
 func get_strength():
 	return strength
@@ -18,23 +16,16 @@ func _physics_process(delta: float) -> void:
 	if player == null:
 		return
 		
-	var to_player = player.global_position - global_position
-	var distance = to_player.length()
-	var dir_to_player = to_player.normalized()
+	var dir_to_player = getDirectionToPlayer()
 
 	# Perpendicular direction for circling
 	var circle_dir = Vector2(-dir_to_player.y, dir_to_player.x)
 
-	# Check if player is looking at enemy
-	var from_player = -dir_to_player
-	var look_dot = player.facing_direction.dot(from_player)
-	var player_looking = look_dot > 0.3
-
 	var move_dir: Vector2
 
-	if player_looking:
+	if playerIsLooking():
 		# Orbit + push back to radius
-		var radius_error = distance - RADIUS
+		var radius_error = getDistanceToPlayer() - RADIUS
 		move_dir = circle_dir + dir_to_player * clamp(radius_error / RADIUS, -1.0, 1.0)
 	else:
 		# Spiral inward
