017b4105922bcaadaa3f6c57937fd3058668009b
walchhoferG <8gibii@gmail.com>
2026-01-31 18:15:01 +0100

    added new lightcone

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -13,8 +13,8 @@ const DURATION := 1.0
 var elapsed := 0.0
 var start_scale_y := 1.0
 
-func _ready():
-	start_scale_y = occluder.scale.y
+#func _ready():
+	#start_scale_y = occluder.scale.y
 
 func _process(delta: float) -> void:
 	if(health._current_health <= 0):
