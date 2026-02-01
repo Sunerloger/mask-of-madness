a3049d63a3facd0a7fcb29326e5fb134d83a8a34
Theo Moosham <178655317+tutabot@users.noreply.github.com>
2026-02-01 13:24:21 +0100

    pickup scripts updated

--- assets/scripts/sanity_pickup.gd
+++ assets/scripts/sanity_pickup.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var leaves_particles: GPUParticles2D = $leaves_particles
+@onready var shadow: Sprite2D = $shadow
+
+func _ready() -> void:
+	sprite_2d.material.light_mode = 2
+	leaves_particles.material.light_mode = 2
+	shadow.material.light_mode = 2
+	modulate.a = 0.0
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN)
+	tween.tween_property(self,"modulate:a", 1.0, 1.0)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
