b93b55bb561cc80a165db788e0a9f1d748f3c726
Elias Anton Rozanics <e12122077@student.tuwien.ac.at>
2026-02-01 13:41:03 +0100

    feat: pickup sound

--- assets/scripts/health_pickup.gd
+++ assets/scripts/health_pickup.gd
@@ -1,10 +1,18 @@
 extends Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+var is_destroyed: bool = false
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not is_destroyed:
+		is_destroyed = true
 		var health := body.get_node("Health")
 		health.update_current_health(5)
+		
+		visible = false
+		audio_stream_player_2d.play()
+		await audio_stream_player_2d.finished
 		queue_free()
 
 
